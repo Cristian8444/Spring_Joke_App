79b3fb1a7cdb8c688c9193c3eee43484a5b612d0
DESKTOP-ULPKO93\Cristian&Raluca <mojo_jojo844@yahoo.com>
2021-08-04 13:37:22 +0300

    New spring project. joke app aplication

---
+++ src/test/java/ciresaru/springframework/joke/com/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package ciresaru.springframework.joke.com.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
